ae8eca407149ddf9100b7af955d04bc19b7871bb
BuildTools <unconfigured@null.spigotmc.org>
2021-08-24 21:24:16 -0700

    Module 1 Complete

--- src/_00_Intro_To_Arrays/_00_ArrayDemo.java
+++ src/_00_Intro_To_Arrays/_00_ArrayDemo.java
@@ -6,39 +6,46 @@ public class _00_ArrayDemo {
         /*
          * Declaring and Initializing an array of Strings
          */
-        String[] names = new String[5];
-        
-        names[0] = "Jan";
-        names[1] = "Sara";
-        names[2] = "Fred";
-        names[3] = "Marc";
-        names[4] = "Randy";
-
-        /*
-         * Declaring and Initializing an array of ints using a '{' and '}'
-         */
-        int[] ints = { 0, 1, 2, 3, 4 };
-        
-        /*
-         * Iterating through array with a standard for-loop
-         */
-        System.out.println("Here's the array of Strings:\n");
-        
-        for(int i = 0; i < names.length; i++){
-            String s = names[i];
-            System.out.println("String at element " + i + ": " + s);
-        }
+//        String[] names = new String[5];
+//        
+//        names[0] = "Jan";
+//        names[1] = "Sara";
+//        names[2] = "Fred";
+//        names[3] = "Marc";
+//        names[4] = "Randy";
+//
+//        /*
+//         * Declaring and Initializing an array of ints using a '{' and '}'
+//         */
+//        int[] ints = { 0, 1, 2, 3, 4 };
+//        
+//        /*
+//         * Iterating through array with a standard for-loop
+//         */
+//        System.out.println("Here's the array of Strings:\n");
+//        
+//        for(int i = 0; i < names.length; i++){
+//            String s = names[i];
+//            System.out.println("String at element " + i + ": " + s);
+//        }
+//        System.out.println("\n");
+//
+//        /*
+//         * Iterating through array with a for-each loop
+//         */
+//        System.out.println("Here's the array of ints:\n");
+//        
+//        for(Integer i : ints){
+//            System.out.println("int: " + i);
+//        }
         System.out.println("\n");
-
-        /*
-         * Iterating through array with a for-each loop
-         */
-        System.out.println("Here's the array of ints:\n");
         
-        for(Integer i : ints){
-            System.out.println("int: " + i);
+        int[][] matrix = {{0,1,2}, {3}, {4, 5}};
+        for(int[] i: matrix) {
+        	for(Integer j:i) {
+        		System.out.print(j);
+        	}
+        System.out.println();
         }
-        System.out.println("\n");
-        
     }
 }
