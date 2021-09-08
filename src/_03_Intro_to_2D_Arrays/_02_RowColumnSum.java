1bd3997ef0b29e816cb36fd22a009cb4a0a60397
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 18:40:15 -0700

    Firework Display Finished

--- src/_03_Intro_to_2D_Arrays/_02_RowColumnSum.java
+++ src/_03_Intro_to_2D_Arrays/_02_RowColumnSum.java
@@ -3,12 +3,18 @@ package _03_Intro_to_2D_Arrays;
 public class _02_RowColumnSum {
 
     public int getSumByRow(int[][] matrix, int row) {
-        
-        return 0;
+    	int x = 0;
+    	for(int i = 0; i < matrix[row].length; i++) {
+    		x += matrix[row][i];
+    	}
+        return x;
     }
     
     public int getSumByColumn(int[][] matrix, int col) {
-        
-        return 0;
+    	int x = 0;
+    	for(int i = 0; i < matrix.length; i++) {
+    		x += matrix[i][col];
+    	}
+        return x;
     }
 }
